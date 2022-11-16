a7d6a7b98a0520b2495eee81bf1fc14a8c439854
queenmo <82431817+queenmo@users.noreply.github.com>
2022-11-16 18:42:34 +0700

    User register and login COMPLETED

--- app/src/main/java/com/example/finalproject2/User/LoginActivity.java
+++ app/src/main/java/com/example/finalproject2/User/LoginActivity.java
@@ -3,11 +3,17 @@ package com.example.finalproject2.User;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.finalproject2.Products.HomepageActivity;
 import com.example.finalproject2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,51 +22,77 @@ import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
-
-    EditText etName;
-    EditText etPassword;
-    TextView tvRegister;
-    Button btnLogin;
-    DatabaseHelper db;
+    private EditText edtEmail, edtPassword;
+    private TextView tvRegister;
+    private Button btnLogin, btnRegister;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etName = findViewById(R.id.etName);
-        etPassword = findViewById(R.id.etPassword);
-        tvRegister = findViewById(R.id.tvRegister);
-        btnLogin = findViewById(R.id.btnLogin);
-        db = new DatabaseHelper(this);
+        initView();
+        login();
+    }
 
+    private void login() {
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
-
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String user = etName.getText().toString().trim();
-                String pwd = etPassword.getText().toString().trim();
-                boolean res = db.checkUser(user, pwd);
-                if (res)
-                {
-                    Toast.makeText(LoginActivity.this, "Login Berhasil!",Toast.LENGTH_SHORT).show();
-                    Intent intent=new Intent(LoginActivity.this,HomepageActivity.class);
-                    startActivity(intent);
-                    finish();
+            public void onClick(View view) {
+                //menampung imputan user
+                final String emailUser = edtEmail.getText().toString().trim();
+                final String passwordUser = edtPassword.getText().toString().trim();
+
+                //validasi email dan password
+                // jika email kosong
+                if (emailUser.isEmpty()) {
+                    edtEmail.setError("Email tidak boleh kosong");
                 }
-                else
-                {
-                    Toast.makeText(LoginActivity.this, "Login Gagal!",Toast.LENGTH_SHORT).show();
+                // jika email not valid
+                else if (!Patterns.EMAIL_ADDRESS.matcher(emailUser).matches()) {
+                    edtEmail.setError("Email tidak valid");
+                }
+                // jika password kosong
+                else if (passwordUser.isEmpty()) {
+                    edtPassword.setError("Password tidak boleh kosong");
+                }
+                //jika password kurang dari 6 karakter
+                else if (passwordUser.length() < 6) {
+                    edtPassword.setError("Password minimal terdiri dari 6 karakter");
+                } else {
+                    auth.signInWithEmailAndPassword(emailUser, passwordUser)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // ketika gagal locin maka akan do something
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this,
+                                                "Gagal login karena " + task.getException().getMessage()
+                                                , Toast.LENGTH_LONG).show();
+                                    } else {
+                                        //jika sukses akan menuju ke homepage activity
+                                        startActivity(new Intent(LoginActivity.this,HomepageActivity.class));
+                                    }
+                                }
+                            });
                 }
             }
         });
     }
 
+    private void initView() {
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        tvRegister = findViewById(R.id.tvRegister);
+        auth = FirebaseAuth.getInstance();
+    }
 }
